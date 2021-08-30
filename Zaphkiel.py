ebb278c47bcb58dd4c41bd0686459222b917a7d5
John Lester L. Licayan <johnlesterincbusiness@gmail.com>
2021-08-31 01:26:51 +0800

    Update Zaphkiel.py

--- Zaphkiel.py
+++ Zaphkiel.py
@@ -1,4 +1,22 @@
 import Network
 
-client = Network.ClientNetwork()
-client.MainScreen()
+animList = [
+    'Aaaaaaaaaa',
+    'aAaaaaaaaa',
+    'aaAaaaaaaa',
+    'aaaAaaaaaa',
+    'aaaaAaaaaa',
+    'aaaaaAaaaa',
+    'aaaaaaAaaa',
+    'aaaaaaaAaa',
+    'aaaaaaaaAa',
+    'aaaaaaaaaA',
+    'aaaaaaaaaa']
+
+finished = False
+
+thread = Network.threading.Thread(target=Network.animate, args=(animList, finished))
+thread.start()
+
+
+finished = True
